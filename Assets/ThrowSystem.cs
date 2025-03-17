a09b1be9bb17d6b79c3121bd4b2b30d51e44299c
nano1412_PC <54862667+nano1412@users.noreply.github.com>
2025-03-17 16:47:35 +0700

    hand now rotate to mouse

--- Assets/ThrowSystem.cs
+++ Assets/ThrowSystem.cs
@@ -1,16 +1,22 @@
 using Unity.Netcode;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class ThrowSystem : NetworkBehaviour
 {
     [SerializeField] NetworkManager networkManager;
     [SerializeField] GameObject bomb;
     [SerializeField] GameObject arrow;
-    public float angle;
+    [SerializeField] GameObject hand;
+
+    private Vector3 mouse_pos;
+    private Vector3 object_pos;
+    private float angle;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        hand = transform.Find("hand").gameObject;
     }
 
     // Update is called once per frame
@@ -18,13 +24,13 @@ public class ThrowSystem : NetworkBehaviour
     {
         if (IsOwner)
         {
-            Vector2 mousePosition = Input.mousePosition;
-            //mousePosition.z = 10.0f;
-            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
-            Vector2 playerPosition = transform.position;
-
-            angle = Vector2.SignedAngle(playerPosition, mousePosition);
+            mouse_pos = Input.mousePosition;
+            mouse_pos.z = -20;
+            object_pos = Camera.main.WorldToScreenPoint(hand.transform.position);
+            mouse_pos.x = mouse_pos.x - object_pos.x;
+            mouse_pos.y = mouse_pos.y - object_pos.y;
+            angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+            hand.transform.rotation = Quaternion.Euler(0, 0, angle);
         }
     }
 }
