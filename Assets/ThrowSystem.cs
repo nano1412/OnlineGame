061e105f94812df9941101eba5349821bf3f9d9c
nano1412_PC <54862667+nano1412@users.noreply.github.com>
2025-03-17 23:54:44 +0700

    move addfore to clientRPC

--- Assets/ThrowSystem.cs
+++ Assets/ThrowSystem.cs
@@ -95,10 +95,18 @@ public class ThrowSystem : NetworkBehaviour
         print(mouse_pos.x);
         print(mouse_pos.y);
         GameObject newBomb = Instantiate(bomb, hand.transform.Find("bombRelease").position, Quaternion.identity,transform);
-        newBomb.GetComponent<NetworkObject>().Spawn(true);
+        //newBomb.GetComponent<NetworkObject>().Spawn(true);
+
+        ThrowClientRpc(newBomb);
         // add force depend on angle of hand and power
 
-        newBomb.GetComponent<Rigidbody2D>().AddForce(new Vector2(mouse_pos.x, mouse_pos.y) * power * powerMultiplier,ForceMode2D.Impulse);
+        
+    }
+
+    [ClientRpc]
+    void ThrowClientRpc(GameObject bomb)
+    {
+        bomb.GetComponent<Rigidbody2D>().AddForce(new Vector2(mouse_pos.x, mouse_pos.y) * power * powerMultiplier, ForceMode2D.Impulse);
         power = 1;
     }
 }
