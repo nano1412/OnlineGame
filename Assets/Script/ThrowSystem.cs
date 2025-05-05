d796a06d3981fe87a10ee604c63f6e4de627e8d5
nano1412_PC <54862667+nano1412@users.noreply.github.com>
2025-05-05 17:14:13 +0700

    control throwpoint via touch screen

--- Assets/Script/ThrowSystem.cs
+++ Assets/Script/ThrowSystem.cs
@@ -4,6 +4,9 @@ using static UnityEngine.GraphicsBuffer;
 
 public class ThrowSystem : NetworkBehaviour
 {
+    [SerializeField] public GameController gameController;
+    private GameObject canvas;
+
     [SerializeField] GameObject hand;
     [SerializeField] GameObject BombReleasePoint;
     [SerializeField] float throwPointDistance;
@@ -14,6 +17,17 @@ public class ThrowSystem : NetworkBehaviour
     private Vector3 mouse_pos;
     private Vector3 object_pos;
     private float angle;
+
+    //for mobile control
+    private bool touchStart = false;
+    private Vector2 pointA;
+    private Vector2 pointB;
+    private float middleOfScreen = Screen.width / 2;
+    public GameObject rotationCirclePrefab;
+    public GameObject rotationOuterCirclePrefab;
+    public GameObject rotationCircle;
+    public GameObject rotationOuterCircle;
+    public float maxFollowRadius;
 
     [SerializeField] GameObject bomb;
     [SerializeField] GameObject arrow;
@@ -34,6 +48,11 @@ public class ThrowSystem : NetworkBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        canvas = GameObject.Find("Canvas");
+
+        rotationCircle = Instantiate(rotationCirclePrefab, canvas.transform);
+        rotationOuterCircle = Instantiate(rotationOuterCirclePrefab, canvas.transform);
+
         defaultHandPostion = hand.transform.localPosition;
         hand = transform.Find("hand").gameObject;
         BombReleasePoint = hand.transform.Find("bombRelease").gameObject;
@@ -55,13 +74,21 @@ public class ThrowSystem : NetworkBehaviour
     {
         if (!IsOwner) return;
 
-        UpdateHand();
-        UpdateThrowPosition();
-        ChargeUp();
+        if (gameController.isPC)
+        {
+            UpdateHandPC();
+            UpdateHand();
+            UpdateThrowPositionPC();
+            ChargeUpPC();
+        } else
+        {
+            UpdateHandMobile();
+            UpdateHand();
+        }
 
     }
 
-    void UpdateHand()
+    void UpdateHandPC()
     {
         if (!IsOwner) return;
 
@@ -73,6 +100,58 @@ public class ThrowSystem : NetworkBehaviour
         mouse_pos.y = mouse_pos.y - object_pos.y;
         angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
 
+        
+    }
+
+    void UpdateHandMobile()
+    {
+        if (!IsOwner) return;
+
+        if (Input.GetMouseButtonDown(0) && Input.mousePosition.x > middleOfScreen)
+        {
+
+            pointA = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+            rotationCircle.transform.position = pointA;
+            rotationOuterCircle.transform.position = pointA;
+            rotationCircle.SetActive(true);
+            rotationOuterCircle.SetActive(true);
+
+        }
+        if (Input.GetMouseButton(0))
+        {
+            touchStart = true;
+            pointB = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+        else
+        {
+            touchStart = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (touchStart && !gameController.isPC && IsOwner)
+        {
+            if (Input.mousePosition.x > middleOfScreen)
+            {
+                Vector2 offset = pointB - pointA;
+                Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+                angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+                rotationCircle.transform.position = new Vector2(pointA.x + offset.x, pointA.y + offset.y);
+            }
+        }
+        else
+        {
+            rotationCircle.SetActive(false);
+            rotationOuterCircle.SetActive(false);
+        }
+
+    }
+
+    private void UpdateHand()
+    {
         if (playermovement.isFlipped.Value)
         {
             hand.transform.rotation = Quaternion.Euler(0, 0, angle + 180);
@@ -85,7 +164,7 @@ public class ThrowSystem : NetworkBehaviour
         }
     }
 
-    void ChargeUp()
+    void ChargeUpPC()
     {
         cooldowncount -= Time.deltaTime;
 
@@ -117,7 +196,7 @@ public class ThrowSystem : NetworkBehaviour
         return (mouseWorld - BombReleasePoint.transform.position).normalized;
     }
 
-    void UpdateThrowPosition()
+    void UpdateThrowPositionPC()
     {
         
         Vector3 direction = mouse_pos - transform.position;
